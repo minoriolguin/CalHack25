67988cc9f840583823f6fc2852e91799592187b1
Jacob McCarthy <jacob.mccarthy@shaw.ca>
2025-02-15 14:22:57 -0700

    Created backend maven springboot project

---
+++ cookproj/src/main/java/com/calhack/cookproj/CalHacks2025ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.calhack.cookproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalHacks2025ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalHacks2025ProjectApplication.class, args);
+	}
+
+}
---
+++ cookproj/src/test/java/com/calhack/cookproj/CalHacks2025ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calhack.cookproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalHacks2025ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
