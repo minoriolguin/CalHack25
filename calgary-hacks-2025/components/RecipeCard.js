8c688d5ca4f9d13e7534930cf16a78da105ae832
Toto <iwaoiboaka@gmail.com>
2025-02-15 21:12:33 -0700

    Update RecipeCard.js

--- calgary-hacks-2025/components/RecipeCard.js
+++ calgary-hacks-2025/components/RecipeCard.js
@@ -15,8 +15,8 @@ return (
       alt={imageAlt}
       layout ="fill"
       objectFit="cover" // Keeps aspect ratio and fills the box
-      className="object-contain w-full aspect-[2.08]"
-    />
+      className="rounded-t-xl"
+      />
     <div className="flex flex-col items-start py-5 pr-20 pl-4 bg-black bg-opacity-0 max-md:pr-5">
       <div className="text-base font-bold">{title}</div>
       <div className="mt-3 text-sm">By Chef {chef}</div>
