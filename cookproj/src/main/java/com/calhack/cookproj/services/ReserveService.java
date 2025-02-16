868aac2a0fe7dcc836a23772cfc6e2f74b76b131
Angie Shin <jeonghee0629@gmail.com>
2025-02-16 03:25:26 -0700

    commit for the reversation

--- cookproj/src/main/java/com/calhack/cookproj/services/ReserveService.java
+++ cookproj/src/main/java/com/calhack/cookproj/services/ReserveService.java
@@ -1,9 +1,14 @@
 package com.calhack.cookproj.services;
 
+import com.calhack.cookproj.models.ClassInfo;
+import com.calhack.cookproj.models.Location;
+import com.calhack.cookproj.models.User;
 import com.calhack.cookproj.repositories.LocationRepository;
 import com.calhack.cookproj.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReserveService {
@@ -14,16 +19,32 @@ public class ReserveService {
     @Autowired
     private UserRepository userRepository;
 
-    /* reserve(String className, LocationName, username) {
-       user = userRepository.findbyusername(username)
-       List<ClassInfo> userlist = user.getclasses
-       find class by getting location based off of parameter
-       get list of classes by using getter
-       itterate through list (or search for it just get and make a copy of the classinfo object that matches the name of the one in the parameter)
-       insert that classinfo object into userlist
-       update userlist in database by useing user.setclasslist thingy
-       i think u need to use userrepo.save(user) after this idk try it out
-       return a conformation string or something maybe the new obejct so we can update we will figure that out later
-    } */
+    public String reserve(String className, String locationName, String username) {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return "User not found";
+        }
 
+        List<ClassInfo> userClasses = user.getSignedUpClasses();
+        Location location = locationRepository.findByName(locationName);
+        if (location == null) {
+            return "Location not found";
+        }
+
+        List<ClassInfo> locationClasses = location.getClasses();
+        ClassInfo classToReserve = locationClasses.stream()
+                .filter(classInfo -> classInfo.getName().equals(className))
+                .findFirst()
+                .orElse(null);
+
+        if (classToReserve == null) {
+            return "Class not found";
+        }
+
+        userClasses.add(classToReserve);
+        user.setSignedUpClasses(userClasses);
+        userRepository.save(user);
+
+        return "Class reserved successfully";
+    }
 }
