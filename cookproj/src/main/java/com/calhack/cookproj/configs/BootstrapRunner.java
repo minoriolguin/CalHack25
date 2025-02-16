fb38dfb39582f25a1bb0f24e00d34817c533f413
Angie Shin <jeonghee0629@gmail.com>
2025-02-16 03:25:26 -0700

    commit for the reversation

--- cookproj/src/main/java/com/calhack/cookproj/configs/BootstrapRunner.java
+++ cookproj/src/main/java/com/calhack/cookproj/configs/BootstrapRunner.java
@@ -1,7 +1,13 @@
 package com.calhack.cookproj.configs;
 
+import com.calhack.cookproj.models.ClassInfo;
+import com.calhack.cookproj.models.Location;
 import com.calhack.cookproj.models.User;
+import com.calhack.cookproj.repositories.LocationRepository;
 import com.calhack.cookproj.repositories.UserRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,8 +16,12 @@ import org.springframework.stereotype.Component;
 public class BootstrapRunner implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final LocationRepository locationRepository;
+
     @Autowired
-    public BootstrapRunner(UserRepository userRepository) { this.userRepository = userRepository; }
+    public BootstrapRunner(UserRepository userRepository, LocationRepository locationRepository) { this.userRepository = userRepository;
+        this.locationRepository = locationRepository;
+    }
 
     @Override
     public void run(String ... args) throws Exception {
@@ -22,6 +32,7 @@ public class BootstrapRunner implements CommandLineRunner {
 
         if (user == null) {
             User newUser = new User();
+            newUser.setSignedUpClasses(List.of());
             newUser.setUsername(adminUsername);
             newUser.setPassword(adminPassword);
             newUser.setRole(User.UserRole.ADMIN);
@@ -39,6 +50,15 @@ public class BootstrapRunner implements CommandLineRunner {
             System.out.println("Admin user updated!");
         } else {
             System.out.println("User already exists!");
+            Location location = new Location();
+            location.setName("Test Location");
+            location.setAddress("1234 Test Street");
+            location.setReviews(4.0);
+            location.setClasses(List.of(
+                    new ClassInfo("YOGA", "Monday", "5:50", 1, 6),
+                    new ClassInfo("HIIT", "Tuesday", "6:30", 2, 7)
+            ));
+            locationRepository.save(location);
         }
     }
 }
