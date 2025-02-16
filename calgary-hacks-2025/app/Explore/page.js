a537fbc92f58d74fcba9f4e811daf58b532f4f28
Angie Shin <jeonghee0629@gmail.com>
2025-02-15 17:48:20 -0700

    added filter button nearby classes and updated the cooking class page

--- calgary-hacks-2025/app/Explore/page.js
+++ calgary-hacks-2025/app/Explore/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
-import FilterButton from "../components/FilterButton";
+import FilterButton from "../components/FilterButtonNearbyClasses";
 import ClassCard from "../components/ClassCard";
 import NearbyClasses from "../components/NearbyClasses";
 
